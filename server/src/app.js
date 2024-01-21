735a3b02b206ee49c6ff4b7f2f045459dd6af8e1
olgagrinko-dev <olgagrinko.87@mail.ru>
2024-01-21 20:19:48 +0300

    swagger

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const route = require('./controller/user.controller');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cookieParser());
 app.use('/user', route);
 app.use((error, req, res, _next) => res.send(error.message));
