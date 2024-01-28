8ab6824323ce3e425608e3df863dcea5c8321312
olgagrinko-dev <olgagrinko.87@mail.ru>
2024-01-28 21:57:41 +0300

    swag

--- server/src/app.js
+++ server/src/app.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cookieParser());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/user', route);
 app.use((error, req, res, _next) => res.send(error.message));
 
